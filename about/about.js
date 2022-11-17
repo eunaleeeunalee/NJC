33bc78b408b5a63dcf07314b3864f8a8edc1a7ed
eunaleeeunalee <eunaleeeunalee@gmail.com>
2022-11-17 21:27:43 +0100

    mobile update

--- about/about.js
+++ about/about.js
@@ -1,8 +1,10 @@
-const p = document.querySelector("#menu");
+const p = document.querySelector(".menu");
 const intro = document.querySelector(".intro")
+const navi = document.querySelector(".navigater")
 
 function openIt(){
     p.classList.toggle("info");
+    navi.classList.toggle("info");
     intro.classList.toggle("info");
     document.getElementById('myaudio').play()
 }
